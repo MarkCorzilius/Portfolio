4154232c1cb0ce6957bdf29b12cba054eb6c356f
Mark Corzilius <marccorzprivate@gmail.com>
2026-09-06 01:08:17 +0200

    feat(portfolio): add CI/CD to skills section

--- components/layout/SkillsMarquee.tsx
+++ components/layout/SkillsMarquee.tsx
@@ -1,6 +1,6 @@
 import Marquee from "react-fast-marquee";
 
-const skills = ["Django", "Python", "React", "TypeScript", "Git", "Docker", "AWS", "Google Cloud", "JavaScript", "HTML", "CSS", "GitHub", "Angular", "Figma"];
+const skills = ["Django", "Python", "React", "TypeScript", "Git", "Docker", "AWS", "Google Cloud", "CI/CD", "JavaScript", "HTML", "CSS", "GitHub", "Angular", "Figma"];
 
 export default function SkillsMarquee() {
   return (
